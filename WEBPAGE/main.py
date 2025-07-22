e6fba2863b87195e5bed703c9479292240f58a31
jacob levy <jacobelilevy@gmail.com>
2025-07-22 16:30:13 +0100

    time table and update location

--- WEBPAGE/main.py
+++ WEBPAGE/main.py
@@ -48,9 +48,87 @@ def get_time_table(user_id):
         """, (timeTableID,))
     return data
 
+def get_alteration(user_id):
+    alteration = DB_interface.get_data(
+        """
+        SELECT 
+            LOCATIONS.LocationName, 
+            ALTERATION.Start, 
+            ALTERATION.End, 
+            ALTERATION.Day, 
+            ALTERATION.Week,
+            ALTERATION.Title
+        FROM
+            ALTERATION
+        JOIN
+            LOCATIONS ON ALTERATION.LocationID = LOCATIONS.LocationID
+        WHERE
+            ALTERATION.UserID = ?""", 
+        (user_id,)
+        )
+    return alteration
+
+def get_combined_timetable(user_id):
+    tt = get_time_table(user_id)
+    alterations = get_alteration(user_id)
+    timetable = []
+
+    for location, start, end, day, week, title in alterations:
+        entry = (
+            day,
+            start,
+            end,
+            title,
+            location,
+            week,
+            "N/A"
+        )
+        timetable.append(entry)
+    for day, start, end, subject, location, week, teacher in tt:
+        entry = (
+            day,
+            start,
+            end,
+            subject,
+            location,
+            week,
+            teacher
+        )
+        timetable.append(entry)
+
+    return sorted(timetable, key=lambda x: (x[5], x[0], x[1]))
+
+def update_current_location(user_id, location):
+    locationID = DB_interface.get_data("SELECT LocationID FROM LOCATIONS WHERE LocationName = ?", (location,))[0][0]
+    now = datetime.now()
+    d = now.weekday()
+    t = now.strftime("%H:%M")
+    w = 1 if (now.isocalendar().week % 2) == 1 else 2
+    DB_interface.execute_query("UPDATE ALTERATION SET LocationID = ?, Start = ?, Day = ?, Week = ? WHERE UserID = ? AND EventID = 1", (locationID, t, d, w, user_id))
+
+@app.route('/update', methods=['GET', 'POST'])
+def update():
+    if request.method == 'POST':
+        location = request.form['location']
+        update_current_location(session['user_id'], location)
+        return redirect(url_for('home'))
+    return render_template('update.html', locations=[i[0] for i in DB_interface.get_data("SELECT LocationName FROM LOCATIONS")])
+
 @app.route('/home', methods=['GET'])
 def home():
-    return render_template('home.html', timeTable=get_time_table(session['user_id']))
+    now = datetime.now()
+    d = now.weekday()
+    t = now.strftime("%H:%M")
+    w = 1 if (now.isocalendar().week % 2) == 1 else 2
+
+    return render_template(
+        'home.html',
+        timeTable=get_combined_timetable(session['user_id']),
+        alteration=get_alteration(session['user_id']),
+        current_day=d,
+        current_time=t,
+        current_week=w
+        )
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -69,7 +147,7 @@ def login():
 
 @app.route('/')
 def redirect_to_login():
-    return redirect("/login", code=302)
+    return redirect("/login")
 
 @app.errorhandler(404)
 def not_found(e):
